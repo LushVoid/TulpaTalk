ce29a390fe8393f0d7f4aca9f4d33685cf5b7dce
Aldan <LushVoid@proton.me>
2023-11-02 17:20:10 -0600

    Remove node_modules from repository

--- node_modules/langchain/dist/tools/google_calendar/create.d.ts
+++
@@ -1,8 +0,0 @@
-import { CallbackManagerForToolRun } from "../../callbacks/manager.js";
-import { GoogleCalendarBase, GoogleCalendarAgentParams } from "./base.js";
-export declare class GoogleCalendarCreateTool extends GoogleCalendarBase {
-    name: string;
-    description: string;
-    constructor(fields: GoogleCalendarAgentParams);
-    _call(query: string, runManager?: CallbackManagerForToolRun): Promise<string>;
-}
